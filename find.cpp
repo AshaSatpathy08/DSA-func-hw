9510a19a61db0c41732e728098ce6cc40a3eb858
Asha <satpathyasharani@gmail.com>
2023-08-03 15:36:26 +0530

    homeWork is done

---
+++ find.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int findNumber(int num){
+    if(num % 2 ==0){
+        cout << "Number is Even";
+    }
+    else{
+        cout << "Number is Odd";
+    }
+    return num;
+}
+
+
+int main(){
+    int num;
+    cout <<" enter a number" <<  endl;
+    cin >> num;
+    
+    findNumber(num);
+
+    return 0;
+}
